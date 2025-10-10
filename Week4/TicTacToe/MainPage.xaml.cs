ee5dad1fd27c7d2b18928a80065e90c5153851f5
Donny <32511652+DonH-ITS@users.noreply.github.com>
2025-10-10 10:45:39 +0100

    Week4 Lecture plus TicTacToe

---
+++ Week4/TicTacToe/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿namespace TicTacToe2025
+{
+    // This has done to the end of DoMove section, just before Checking the filled squares
+    public partial class MainPage : ContentPage
+    {
+        private bool gridcreated = false;
+        private int numbrows = 3;
+        private int player = 1;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void StartBtn_Clicked(object sender, EventArgs e) {
+            if (!gridcreated) {
+                CreateTheGrid();
+                Button mybtn = (Button)sender;
+                mybtn.IsEnabled = false;
+            }
+        }
+
+        private void CreateTheGrid() {
+            //Create numbrows rows and numbrows columns 3x3, 4x4 etc.
+            for (int i = 0; i < numbrows; ++i) {
+                GridPageContent.AddRowDefinition(new RowDefinition());
+                GridPageContent.AddColumnDefinition(new ColumnDefinition());
+            }
+
+            //Populate the grid with Borders
+            for (int i = 0; i < numbrows; ++i) {
+                for (int j = 0; j < numbrows; ++j) {
+                    Border styledBorder = new Border
+                    {
+                        BackgroundColor = Colors.Red,
+                        Stroke = Colors.Black,
+                        StrokeThickness = 3
+
+                    };
+                    TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
+                    tapGestureRecognizer.Tapped += OnBorderTapped;
+                    styledBorder.GestureRecognizers.Add(tapGestureRecognizer);
+                    GridPageContent.Add(styledBorder, j, i);
+                }
+            }
+            gridcreated = true;
+            whichplayerlabel.Text = "Player 1's Turn";
+        }
+
+        private void OnBorderTapped(object? sender, TappedEventArgs e) {
+            Border border = (Border)sender;
+            if (border != null) {
+                DoMove(border);
+            }
+        }
+
+        private void DoMove(Border border) {
+            if(player == 1) {
+                border.BackgroundColor = Color.FromRgb(0, 255, 0);
+                player = 2;
+            }
+            else {
+                border.BackgroundColor = Color.FromRgb(0, 0, 255);
+                player = 1;
+            }
+            whichplayerlabel.Text = "Player " + player + "'s Turn";
+        }
+
+    }
+}
---
+++ Week4/Week4Lecture/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Maui.Dispatching;
+
+namespace AwaitAsyncApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private void SyncBtn_Clicked(object sender, EventArgs e) {
+            lblupd.Text = "Beginning task delay";
+            var t = Task.Delay(6000);
+            t.Wait();
+            lblupd.Text = "I was updated by the Sync Method \u2620";
+        }
+
+        private async void ASyncBtn_Clicked(object sender, EventArgs e) {
+            Button btn = (Button)sender;
+            btn.IsEnabled = false;
+            lblupd.Text = "asynchronous running \u26F9";
+            await Task.Delay(3000);
+            await btn.FadeTo(0, 3000);
+            lblupd.Text = "Updated by Asynchronous Method \u263A";
+// btn.Opacity = 1;
+            btn.IsEnabled = true;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e) {
+            await DisplayAlert("Alert", "You have been alerted", "OK");
+
+            bool yesno = await DisplayAlert("Alert", "Yes or No", "Yes", "No");
+            if (yesno) {
+                lblupd.Text = "clicked Yes";
+                return;
+            }
+            else {
+                lblupd.Text = "Clicked no";
+            }
+            string result = await DisplayPromptAsync("Question 1", "What's your name?");
+
+            if(result != null) 
+                await DisplayAlert("Hello", "Hello " + result, "Good Bye");
+            else 
+                await DisplayAlert("Alert", "Why you no give name", "\u2639");
+        }
+
+    }
+
+}
