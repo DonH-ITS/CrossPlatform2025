b7632a196df374ece6b878fb3cb8ab59e41a2fab
Donny <32511652+DonH-ITS@users.noreply.github.com>
2025-10-15 15:11:05 +0100

    week5drawing

---
+++ Week5/drawing/CustomDrawable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5Lecture
+{
+    public class CustomDrawable : IDrawable
+    {
+        public int CircleSize { get; set; }
+        public int Height { get; set; } = 50;
+        public int Width { get; set; } = 50;
+        public bool DoDraw { get; set; } = true;
+        public void Draw(ICanvas canvas, RectF dirtyRect) {
+            canvas.StrokeColor = Colors.Black;
+            canvas.StrokeSize = 2;
+            if (DoDraw) {
+                // Draw a rectangle
+                canvas.DrawRectangle(50, 50, Width, Height);
+
+                // Draw a circle
+                canvas.FillColor = Colors.Blue;
+                canvas.FillCircle(150, 150, CircleSize);
+            }
+        }
+    }
+}
---
+++ Week5/drawing/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace Week5Lecture
+{
+    public partial class MainPage : ContentPage
+    {
+        private CustomDrawable _customdrawable;
+        public MainPage()
+        {
+            InitializeComponent();
+            _customdrawable = new CustomDrawable();
+            _customdrawable.CircleSize = 100;
+            DrawingCanvas.Drawable = _customdrawable;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e) {
+            // Clear Canvas
+            _customdrawable.DoDraw = false;
+            DrawingCanvas.Invalidate();
+        }
+
+        private void Circle_Clicked(object sender, EventArgs e) {
+            // Draw Rectangle
+            int width;
+            if (!int.TryParse(WidthRect.Text, out width))
+                return;
+            _customdrawable.CircleSize = width;
+            _customdrawable.DoDraw = true;
+            DrawingCanvas.Invalidate();
+
+        }
+
+        private void DrawRectangle_Clicked(object sender, EventArgs e) {
+            // Draw Rectangle
+            int width, height;
+            if (!int.TryParse(WidthRect.Text, out width) || !int.TryParse(HeightRect.Text, out height))
+                return;
+            _customdrawable.Height = height;
+            _customdrawable.Width = width;
+            _customdrawable.DoDraw = true;
+            DrawingCanvas.Invalidate();
+        }
+    }
+}
