c3033521a96b849535f1fbce6fc39b36340257e7
Donny <32511652+DonH-ITS@users.noreply.github.com>
2025-11-14 10:38:37 +0000

    Week8 Lecture notes

---
+++ Week8/DataBindingViewModel/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace ViewModelExample
+{
+    public partial class MainPage : ContentPage
+    {
+        private MyViewModel viewModel;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            viewModel = new MyViewModel();
+            BindingContext = viewModel;
+        }
+
+        private void ChangeText_Clicked(object sender, EventArgs e) {
+            viewModel.ModelText = "New Text";
+        }
+
+        private void Button_Clicked(object sender, EventArgs e) {
+            viewModel.StartStopTimer();
+        }
+
+        private void AddBookButton_Clicked(object sender, EventArgs e) {
+            Int32.TryParse(publishedyear.Text, out int year);
+            viewModel.AddBooks(title.Text, author.Text, year);
+        }
+    }
+}
---
+++ Week8/DataBindingViewModel/MyViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Security.Cryptography;
+
+
+namespace ViewModelExample
+{
+    public class MyViewModel : INotifyPropertyChanged
+    {
+        private System.Timers.Timer _timer;
+        private int _id = 4;
+        private int _seconds = 0;
+        private string _modelText = "";
+        private bool _isRunning = false;
+
+        public ObservableCollection<Book> Books { get; set; }
+        
+        public string ModelText
+        {
+            get
+            {
+                return _modelText;
+            }
+            set
+            {
+                if (_modelText != value) {
+                    _modelText = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int Seconds
+        {
+            get => _seconds;
+            set
+            {
+                _seconds = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+            set
+            {
+                _isRunning = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(StartStop));
+            }
+        }
+
+        public string StartStop
+        {
+            get
+            {
+                if (IsRunning)
+                    return "Stop Timer";
+                else
+                    return "Start Timer";
+            }
+        }
+
+        public MyViewModel() {
+            _timer = new System.Timers.Timer
+            {
+                Interval = 1000
+            };
+            _timer.Elapsed += (s, e) =>
+            {
+                ++Seconds;
+            };
+            Books = new ObservableCollection<Book>
+              {
+              new Book { Id = 1, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", PublishedYear = 1925 },
+              new Book { Id = 2, Title = "1984", Author = "George Orwell", PublishedYear = 1949 },
+              new Book { Id = 3, Title = "To Kill a Mockingbird", Author = "Harper Lee", PublishedYear = 1960 }
+              };
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void StartStopTimer() {
+            if (IsRunning)
+                _timer.Stop();
+            else
+                _timer.Start();
+            IsRunning = !IsRunning;
+        }
+
+        public void AddBooks(string title, string author, int year) {
+            Books.Add(new Book { Id = _id++, Title = title, Author = author, PublishedYear = year });
+           
+        }
+
+        protected virtual void OnPropertyChanged(string? propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
